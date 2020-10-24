2cf1ef9126efb489a6a3dd59d5087ecdce63039b
Kosta <k.v.dinkov@gmail.com>
2020-10-24 20:43:49 +0300

    add: cards/all, cards/collection/ and cards/add open correctly

---
+++ BattleCards/Controllers/CardsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BattleCards.Services;
+using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace BattleCards.Controllers
+{
+    class CardsController:Controller
+    {
+        private readonly ICardService cardService;
+
+        public CardsController(ICardService cardService)
+        {
+            this.cardService = cardService;
+        }
+
+        public HttpResponse All()
+        {
+            if (IsUserSignedIn())
+            {
+                return View();
+            }
+
+            return Redirect("/Users/Login");
+        }
+
+        public HttpResponse Collection()
+        {
+            if (IsUserSignedIn())
+            {
+                return View();
+            }
+            return Redirect("/Users/Login");
+        }
+
+        public HttpResponse Add()
+        {
+            if (IsUserSignedIn())
+            {
+                return View();
+            }
+            return Redirect("/Users/Login");
+        }
+
+    }
+}
--- BattleCards/Controllers/HomeController.cs
+++ BattleCards/Controllers/HomeController.cs
@@ -8,6 +8,10 @@
         [HttpGet("/")]
         public HttpResponse Index()
         {
+            if (IsUserSignedIn())
+            {
+                return Redirect("/Cards/All");
+            }
             return this.View();
         }
         [HttpGet("/Logout")]
