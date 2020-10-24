783869c291ee6a22947c0e81f41f5db42a041b1d
Kosta <k.v.dinkov@gmail.com>
2020-10-24 19:23:22 +0300

    add: register and login pages load correctly

---
+++ BattleCards/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace BattleCards.Controllers
+{
+    class UsersController:Controller
+    {
+
+        public HttpResponse Login()
+        {
+            if (IsUserSignedIn())
+            {
+                return Redirect("/Cards/All");
+            }
+            return this.View();
+        }
+
+        public HttpResponse Register()
+        {
+            if (IsUserSignedIn())
+            {
+                return Redirect("/Cards/All");
+            }
+            return this.View();
+        }
+    }
+}
